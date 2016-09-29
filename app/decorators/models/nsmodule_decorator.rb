9ac64a76848a91be8b87c782ee5c5378e05b6e82
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-29 02:36:41 -0700

    update module contact (top line info + bottom info box)

---
+++ app/decorators/models/nsmodule_decorator.rb
@@ -0,0 +1,8 @@
+Naturesoft::Nsmodule.class_eval do
+  @hotel_newdiscovery_vn_positions = {
+    "header-top-line" => nil,
+    "footer-box-col-1" => nil,
+    "footer-box-col-2" => nil,
+    "footer-box-col-3" => nil,
+  }
+end
